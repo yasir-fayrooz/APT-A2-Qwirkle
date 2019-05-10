4984331500ed19b1e6f1ed67a6925bce0024a605
IdoStudent <41351620+IdoStudent@users.noreply.github.com>
2019-05-10 10:22:33 +1000

    compiling fixes

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -11,18 +11,18 @@ LinkedList::LinkedList() {
 LinkedList::~LinkedList() {
 }
 
-int LinkedList::size()
+int LinkedList::getSize()
 {
 	return size;
 	//size is an instance variable in LinkedList.h file
 }
 
-Tile LinkedList::get(int i)
-{
-	//gets the tile at i position and returns it
-	//does NOT increment or decrement size
-	return null;
-}
+// Tile LinkedList::get(int i)
+// {
+// 	//gets the tile at i position and returns it
+// 	//does NOT increment or decrement size
+// 	return nullptr;
+// }
 
 void LinkedList::addBack(Tile tile)
 {
@@ -46,22 +46,22 @@ bool LinkedList::tileExists(char color, int shape)
 {
 	//iterate through list, if same color and shape set exists to true and return valid
 	bool exists = false;
-	
+
 	return exists;
 }
 
 void LinkedList::shuffleTiles(int index, int randomNumber)
 {
 	/*
-	
+
 	Tile* tile1 = get(index);
 	Tile* tile2 = get(randomNumber);    //We are getting pointers NOT COPYING TILES
-	
+
 	Tile* tempPointer = &tile2;
-	
+
 	tile2 = &tile1;
 	tile1 = &tempPointer;
-	
+
 		//SWAP THE TILES POSITIONS IN THE LINKED LIST
 		//I THINK THATS HOW ITS DONE DOUBLE CHECK WHEN YOU WRITE IT
 	*/
