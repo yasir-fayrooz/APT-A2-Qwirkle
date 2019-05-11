c87fdeded128fd0350f1679a73bbbf7a7954c1f8
shadowhnightxr <41469072+shadowhnightxr@users.noreply.github.com>
2019-05-11 13:05:12 +1000

    qwirkle.cpp fixes
    1.  Lines 16,17 don't need std:: anymore
    2. Line 24 should be while(quitGame == false) because the boolean is already defined within the main method.
    3. Line 36, 40, 44 need to take in the render object as a parameter.
    4. Line 48 is meant to set quitGame = quit() (refer to point no.10)
    5. Line 60 is meant to take in only the render pointer.
    6.  Line 80 should instantiate a new GameEngine object. At the moment, it's using the parameter object which doesn't exist.
    7. Line 85 doesn't need std::
    8. Line 91 only takes the render object as a parameter and initiates a new GameEngine object at line 105.
    9. Line 122 doesn't need std::
    10. Line 130 just returns true;  (refer to point no.4)
    11. Indentation needs to be fixed up a bit as it is a bit inconsistent.

--- qwirkle.cpp
+++ qwirkle.cpp
@@ -13,103 +13,103 @@ void newGame();
 void loadGame();
 void showStudentInfo();
 bool quit();
-bool playerValidation(std::string name);
-void fileValidation(std::string fileName);
+bool playerValidation(string name);
+void fileValidation(string fileName);
 
 int main(void) {
-
+  
   Renderer* render = new Renderer(); // instance
   bool quitGame = false;
-
-	while(bool quitGame = false)
-	{
-	render->clearConsole();
-	render->mainMenu();
-	
-				
-				//This method will render the main menu
-
-	string input = render->getInput();
-					//This method will get input and return the string.
-    	if (input.compare("1"))
-    	{
-    		newGame();
-    	}
-    	else if (input.compare("2"))
-    	{
-    		loadGame();
-    	}
-    	else if (input.compare("3"))
-    	{
-    		showStudentInfo();
-    	}
-    	else if (input.compare("4"))
-    	{
-    		quit();
-    	}
-    	else
-    	{
-    		throw std::runtime_error("Not valid Entry");
-    	}
-
+  
+  while(quitGame == false)
+  {
+    render->clearConsole();
+    render->mainMenu();
+    
+    
+    //This method will render the main menu
+    
+    string input = render->getInput();
+    //This method will get input and return the string.
+    if (input.compare("1"))
+    {
+      newGame(render);
+    }
+    else if (input.compare("2"))
+    {
+      loadGame(render);
+    }
+    else if (input.compare("3"))
+    {
+      showStudentInfo(render);
+    }
+    else if (input.compare("4"))
+    {
+      quitGame = quit();
+    }
+    else
+    {
+      throw std::runtime_error("Not valid Entry");
+    }
+    
     std::cout << "Welcome to Qwirkle!" << "\n" << "-------------------" << "\n";
   }
-   return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
 
-void newGame(Renderer* render, GameEngine* gameEngine)
+void newGame(Renderer* render)
 {
-    render->clearConsole();
-    string player1 = 0;
-    string player2 = 0;
-    
-    while (playerValidation(player1) == false && render->getQuit() == false)
-    {
-    	render->playerName("Player 1"); //This will render player 1 to enter his name
-    	player1 = render->getInput();
-    }
-    
-    while (playerValidation(player2) == false && render->getQuit() == false)
-    {
-    	render->playerName("Player 2"); //This will render player 2 to enter his name
-    	player2 = render->getInput();
-    }
-    
-    if(render->getQuit() == false)
-    {
-    	gameEngine = new GameEngine(player1, player2);
-    	gameEngine->startGame(render);
-    }
+  render->clearConsole();
+  string player1 = 0;
+  string player2 = 0;
+  
+  while (playerValidation(player1) == false && render->getQuit() == false)
+  {
+    render->playerName("Player 1"); //This will render player 1 to enter his name
+    player1 = render->getInput();
+  }
+  
+  while (playerValidation(player2) == false && render->getQuit() == false)
+  {
+    render->playerName("Player 2"); //This will render player 2 to enter his name
+    player2 = render->getInput();
+  }
+  
+  if(render->getQuit() == false)
+  {
+    GameEngine* gameEngine = new GameEngine(player1, player2);
+    gameEngine->startGame(render);
+  }
 }
 
-bool playerValidation(std::string name)
+bool playerValidation(string name)
 {
-	//validates player name to specification after getting input...
-	return false;
+  //validates player name to specification after getting input...
+  return false;
 }
 
-void loadGame(Renderer* render, GameEngine* gameEngine)
+void loadGame(Renderer* render)
 {
-   render->clearConsole();
-   String fileName = 0;
-    
-   while(fileValidation(fileName) == false && render->getQuit() == false)
-   {
-     render->loadGame();
-			//This will render line to enter file name to load
-     fileName = render->getInput();
-   }
-    
-   if(render->getQuit() == false)
-   {
-     gameEngine = new GameEngine(fileName);
-     gameEngine.startGame(render);
-   }
+  render->clearConsole();
+  String fileName = 0;
+  
+  while(fileValidation(fileName) == false && render->getQuit() == false)
+  {
+    render->loadGame();
+    //This will render line to enter file name to load
+    fileName = render->getInput();
+  }
+  
+  if(render->getQuit() == false)
+  {
+    GameEngine* gameEngine = new GameEngine(fileName);
+    gameEngine.startGame(render);
+  }
 }
 
 void showStudentInfo(Renderer* render)
 {
-  render->clearConsole(); 
+  render->clearConsole();
   // clear console
   
   render->showStudentInfo();
@@ -119,15 +119,15 @@ void showStudentInfo(Renderer* render)
   // return to main menu
 }
 
-void fileValidation(std::string fileName)
+void fileValidation(string fileName)
 {
-	//checks if relative file path name exists
-	return false;
+  //checks if relative file path name exists
+  return false;
 }
 
 bool quit()
 {
-	bool quitGame = true;
+  return true;
 }
 
 
@@ -139,30 +139,30 @@ private boolean quitGame = false;
 /* main() method implementation pseudo code:
 
 WHILE (quitGame == false)
-	
-	render->clearConsole();
-	render->mainMenu();
 
-			//This method will render the main menu
+render->clearConsole();
+render->mainMenu();
 
-	String input = render->getInput();
-					//This method will get input and return the string.
+//This method will render the main menu
 
-	IF input == 1
-		newGame();
-		//This method will be in the qwirkle class for further explanation.
-	ELSE IF input == 2
-		loadGame();
-		//This method will be in the qwirkle class for further explanation.
-	ELSE IF input == 3
-		showStudentInfo();
-		//This method will be in the qwirkle class for further explanation.
-	ELSE IF input == 4
-		quit();
-		//This method will be in the qwirkle class for further explanation.
-	ELSE
-		print error blah blah...
-	
+String input = render->getInput();
+//This method will get input and return the string.
+
+IF input == 1
+newGame();
+//This method will be in the qwirkle class for further explanation.
+ELSE IF input == 2
+loadGame();
+//This method will be in the qwirkle class for further explanation.
+ELSE IF input == 3
+showStudentInfo();
+//This method will be in the qwirkle class for further explanation.
+ELSE IF input == 4
+quit();
+//This method will be in the qwirkle class for further explanation.
+ELSE
+print error blah blah...
+
 ENDWHILE
 */
 
@@ -171,16 +171,16 @@ render->clearConsole();
 String player1 = null;
 String player2 = null;
 WHILE (playerValidation(player1) == false && render->getQuit() == false)
-	render->playerName("Player 1"); //This will render player 1 to enter his name
-	player1 = render->getInput();
+render->playerName("Player 1"); //This will render player 1 to enter his name
+player1 = render->getInput();
 ENDWHILE
 WHILE (playerValidation(player2) == false && render->getQuit() == false)
-	render->playerName("Player 2"); //This will render player 2 to enter his name
-	player2 = render->getInput();
+render->playerName("Player 2"); //This will render player 2 to enter his name
+player2 = render->getInput();
 ENDWHILE
 IF(render->getQuit() == false)
-	GameEngine gameEngine = new GameEngine(player1, player2);
-	gameEngine.startGame(render);
+GameEngine gameEngine = new GameEngine(player1, player2);
+gameEngine.startGame(render);
 ENDIF
 */
 
@@ -188,13 +188,13 @@ ENDIF
 render->clearConsole();
 String fileName = null;
 WHILE (fileValidation(fileName) == false && render->getQuit() == false)
-	render->loadGame();
-			//This will render line to enter file name to load
-	fileName = render->getInput();
+render->loadGame();
+//This will render line to enter file name to load
+fileName = render->getInput();
 ENDWHILE
 IF(render->getQuit() == false)
-	gameEngine = new GameEngine(fileName);
-	gameEngine.startGame(render);
+gameEngine = new GameEngine(fileName);
+gameEngine.startGame(render);
 ENDIF
 */
 
